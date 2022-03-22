2e3b80b966ff7849cad552bd5d31181394e56128
hussientriki <101502180+hussientriki@users.noreply.github.com>
2022-03-22 10:34:30 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,26 @@
+from __future__ import division
+
+
+num = int(input("enter the first number : "))
+num1 = int(input("enter the second number : "))
+print("1)addition")
+print("2)subtraction")
+print("3)multiplication")
+print("4)devision")
+print("5)power")
+addition = 1
+subtraction = 2
+multiplication = 3
+division = 4
+power = 5
+sign = int(input("enter the sign : "))
+if sign == addition:
+    print(num + num1)
+elif sign == subtraction:
+    print(num - num1)
+elif sign == multiplication:
+    print(num * num1)
+elif sign == division:
+    print(num / num1)
+elif sign == power:
+    print(num ** num1)
